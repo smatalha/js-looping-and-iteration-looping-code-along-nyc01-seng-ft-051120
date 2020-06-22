5a1dba4a00c0d44b26a51e3fcdfbf96241e30f32
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-06-22 12:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 array = ["Ada", "Brendan", "Ali"]
+
 writeCards(array, "birthday"){
   for (var i = 0; i < array.length; i++) {
     console.log(`Thank you ${array[i]}`);
